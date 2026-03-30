bd6653db19053f376269f024e59e8a5bb69f1ab7
Giannis Kafousis <g.kafousis@gmail.com>
2026-03-30 17:40:35 +0300

    ModulithModularityTests

--- src/test/java/com/demo/ModulithModularityTests.java
+++ src/test/java/com/demo/ModulithModularityTests.java
@@ -2,12 +2,25 @@ package com.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 class ModulithModularityTests {
     static ApplicationModules modules = ApplicationModules.of(ModulithDemoApplication.class);
 
     @Test
+    void printModules() {
+        modules.forEach(System.out::println);
+    }
+
+    @Test
     void verifyModules() {
         modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        new Documenter(modules)
+                .writeDocumentation()
+                .writeIndividualModulesAsPlantUml();
     }
 }
