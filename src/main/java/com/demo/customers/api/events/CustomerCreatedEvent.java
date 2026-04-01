e7cc466dcd1e133b0b3b45d0ee7bd553dd5b79af
Giannis Kafousis <g.kafousis@gmail.com>
2026-04-01 13:25:00 +0300

    public events javadoc

--- src/main/java/com/demo/customers/api/events/CustomerCreatedEvent.java
+++ src/main/java/com/demo/customers/api/events/CustomerCreatedEvent.java
@@ -1,3 +1,9 @@
 package com.demo.customers.api.events;
 
+/**
+ * Event representing the creation of a new customer.
+ *
+ * @param fullName the full name of the customer
+ * @param email the email address of the customer
+ */
 public record CustomerCreatedEvent(String fullName, String email) { }
--- src/main/java/com/demo/customers/api/events/CustomerUpdatedEvent.java
+++ src/main/java/com/demo/customers/api/events/CustomerUpdatedEvent.java
@@ -1,3 +1,9 @@
 package com.demo.customers.api.events;
 
+/**
+ * Event representing the update of an existing customer.
+ *
+ * @param fullName the full name of the customer
+ * @param email the email address of the customer
+ */
 public record CustomerUpdatedEvent(String fullName, String email) { }
